f6f15e709c4b36796ada5602a51c164b5cb3e15f
nataliew <natalie@natalies-mbp-2.attlocal.net>
2019-07-29 23:30:42 -0400

    clean up server.js

--- server.js
+++ server.js
@@ -1,25 +1,36 @@
+// This is the Node server that listens for get request
 var express = require('express');
 var bodyParser = require('body-parser');
 var app     = express();
 const path = require('path');
-var pgp = require('pg-promise')(/*options*/)
+
+// opening connection to the database
+var pgp = require('pg-promise')()
 var db = pgp(process.env.DATABASE_URL)
 const { Pool } = require('pg');
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL
 });
 
+// connects to database
 pool.on('connect', () => {
   console.log('connected to the db');
 });
 
-//Note that in version 4 of express, express.bodyParser() was
-//deprecated in favor of a separate 'body-parser' module.
-app.use(bodyParser.urlencoded({ extended: true }));
+// lisens on port set as environment variable or on 8080
+app.listen(process.env.PORT || 8080, function() {
+    console.log('Server running');
+  });
 
-app.use(express.static(path.join(__dirname, 'public')));
 
+// response for homepage
+app.get('/',function(req,res) {
+    res.sendFile(path.join(__dirname+'/index.html'));
+  });
+
+// Parse information from form and sets variables
+app.use(bodyParser.urlencoded({ extended: true })); //for reading info from form
+app.use(express.static(path.join(__dirname, 'public'))); //for displaying CSS
 app.post('/myaction', function(req, res) {
     var rain = false;
     var snow = false;
@@ -29,8 +40,9 @@ app.post('/myaction', function(req, res) {
     if (req.body.snow == "snow") {
       snow = true;
     }
+
+// Creates query to send form data to database
     const queryText = 'INSERT INTO public."userData" VALUES($1, $2, $3, $4, $5, $6, $7, $8);'
-    //const queryText = 'SELECT * FROM public."userData";'
     const values = [req.body.zip, req.body.cold, req.body.hot, rain, snow, null, req.body.name, req.body.phone]
   pool.query(queryText, values)
     .then((res) => {
@@ -42,16 +54,8 @@ app.post('/myaction', function(req, res) {
       pool.end();
     });
 
-    console.log('test');
+
+// Displays response to user to let them know that their info was submitted
     var response = 'Thanks for your information! You will get weather updates by text.'
-
     res.send(response);
-});
-app.get('/',function(req,res) {
-  res.sendFile(path.join(__dirname+'/index.html'));
-});
-
-
-app.listen(process.env.PORT || 8080, function() {
-  console.log('Server running at http://127.0.0.1:8080/');
 });
